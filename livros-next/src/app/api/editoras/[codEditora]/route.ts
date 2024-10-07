7e2e3ee30afaa52815625f7fbda7196a803ac805
claudio-py <claudio.3777@gmail.com>
2024-10-07 14:54:17 -0300

    treat :any type errors

--- livros-next/src/app/api/editoras/[codEditora]/route.ts
+++ livros-next/src/app/api/editoras/[codEditora]/route.ts
@@ -1,16 +1,19 @@
 import { NextResponse } from 'next/server';
 import { controleEditora } from '../route';
-// import { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
 
-interface Context {
-  params: {
-    codEditora: string;
-  };
-}
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const codEditora = parseInt(searchParams.get('codEditora') || '0', 10);
 
-export async function GET(context: Context) {
-  const { params } = context;
-  const codEditora = parseInt(params.codEditora, 10);
-  const editoras = controleEditora.getNomeEditora(codEditora);
-  return NextResponse.json(editoras);
+  if (isNaN(codEditora) || codEditora <= 0) {
+    return NextResponse.json({ mensagem: 'Invalid codEditora' }, { status: 400 });
+  }
+
+  try {
+    const nomeEditora = controleEditora.getNomeEditora(codEditora);
+    return NextResponse.json({ nome: nomeEditora });
+  } catch (error) {
+    return NextResponse.json({ mensagem: (error as Error).message }, { status: 500 });
+  }
 }
